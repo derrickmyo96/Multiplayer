591aec2e42aabe33093b5b2351dc204af749adff
derrickmyo96 <derrick.myo.oo@gmail.com>
2019-04-04 22:07:02 +0800

    First commit

---
+++ Assets/My Scripts/PlayerNetwork.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    public GameObject firstPersonCharacter;
+    public GameObject[] characterModel;
+
+    public PlayerNetwork()
+    {
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<FirstPersonController>().enabled = true;
+        firstPersonCharacter.SetActive(true);
+
+        foreach (GameObject go in characterModel)
+        {
+            go.SetActive(false);
+        }
+    }
+}
---
+++ Assets/My Scripts/Player_Animation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Animation : NetworkBehaviour
+{
+    public Animator playerAnimator;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForPlayerInput();
+    }
+
+    void CheckForPlayerInput()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0 || Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            playerAnimator.SetBool("Moving", true);
+        }
+
+        else
+        {
+            playerAnimator.SetBool("Moving", false);
+        }
+    }
+}
